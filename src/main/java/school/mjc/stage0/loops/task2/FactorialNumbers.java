99334eb52e75aaef65b5a94b132302fc1d9ef141
Sergey234 <mostov.sergey88@gmail.com>
2022-11-23 16:21:43 +0300

    stage0-module4-loops-task2

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -1,14 +1,10 @@
 package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
-    public void main(String[] args) {
-        printFactorialRow(7);
-    }
-
     public void printFactorialRow(int printToInclusive) {
         int b = 0;
         while (b <= printToInclusive) {
-
+            b++;
         }
     }
 }
